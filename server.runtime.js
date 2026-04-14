ee9329869d1a3e363f00fe543140607ef3cee8f3
Jhanak Singh Lodhi <jhanak@Jhanaks-MacBook-Air.local>
2026-04-14 03:32:41 -0700

    Align server startup and health route for Render

--- server.runtime.js
+++ server.runtime.js
@@ -111,7 +111,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-  res.send('Server is working 🚀');
+  res.send('Server running');
 });
 app.get('/app', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/properties', (req, res) => res.sendFile(path.join(__dirname, 'public', 'properties.html')));
@@ -368,7 +368,7 @@ mongoose.connect(process.env.MONGO_URI)
     const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => {
-      console.log('Server running on port ' + PORT);
+      console.log('Server started on port ' + PORT);
     });
   })
   .catch((err) => {
